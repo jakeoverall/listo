2cc91cad646109d5470b057a3574fd942cdeb320
eanplatter <ean@eanplatter.com>
2014-09-10 23:47:34 -0600

    omg

--- app/js/app.js
+++ app/js/app.js
@@ -1,47 +1,80 @@
-$(document).ready(function() {
+$(document).on('ready', function() {
 	$('#newTaskForm').hide();
 	
 	var newListArray = [];
 	var inProgressArray = [];
 	var finishedArray = [];
+	var totalTasks = 0;
 
-	$('#newListItem').click(function() {
+	var Task = function(task){
+		this.task = task;
+		this.id = totalTasks += 1;
+	};
+
+	Task.prototype.start = function(){
+		debugger;
+		for(var i = 0; i < newListArray.length; i++) {
+			if(newListArray[i].id === this.id) {
+				newListArray.splice(i, 1);
+				inProgressArray.push(this);
+				break;
+			}
+		}
+	} 
+
+	$('#newListItem').on('click',function() {
 		$('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
 	});
 
-	$('#cancel').click(function(e) {
+	$('#cancel').on('click', function(e) {
 		e.preventDefault();
 		$('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
 	});
 
-	$('#saveNewItem').click(function(e) {
+	var newObj = {};
+
+	$('#saveNewItem').on('click', function(e) {
 		e.preventDefault();
 		var newItem = $('#newItemInput').val();
-		var newObj = {
-			body: newItem,
-			id: newListArray.length,
-			startTask: function(){				
-				newListArray.splice(this.id, 1)
-			}
-		};
+		newObj = new Task(newItem);
+	
+
 		if(newItem) {
 			newListArray.push(newObj);
 			console.log(newListArray);
-			$('#newList').append('<li class="list-group-item" id="item">' + newObj.body + '<a href="#start" class="pull-right" id="startTask">Start &#x2192;</a></li>')
+			$('#newList').append('<li class="list-group-item" id="item">' + newObj.task + '<a href="#start" class="pull-right" id="startTask">Start &#x2192;</a></li>');
 			$('#newItemInput').val('');
 			$('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');			
 		}
 	});
 
-	$('#startTask').click(function(task){
 
+
+
+
+
+
+	 $(document).on('click', '#startTask', function() {
+		$('#startTask').on('click', function(e) {
+			 e.preventDefault();
+			 debugger;
+			 this.remove();
 	});
+		
+});	
 
-	$('#startTask').click(function(e){
-		debugger;
-		e.preventDefault();
-		inProgressArray.push(e)
-	});
+ 	// $(document).on('click', '#startTask', function() {
 
+// 		$('#startTask').on('click',function(){
+// 			debugger;
+// 			newObj.startTask();
 
-});
+// 			console.log(inProgressArray);
+// 		});
+// 	});
+// });
+	// $('#startTask').click(function(e){
+	// 	debugger;
+	// 	e.preventDefault();
+	// 	inProgressArray.push(e)
+ });
