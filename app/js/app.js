50b5aa71926009e2fc7f762fca9a3445902e1a7b
Jake Overall <joverall22@gmail.com>
2014-09-16 00:04:16 -0600

    Adds local storage to Listo

--- app/js/app.js
+++ app/js/app.js
@@ -1,84 +1,126 @@
-$(document).on('ready', function() {
-	$('#newTaskForm').hide();
-	
-	var newListArray = [];
-	var inProgressArray = [];
-	var finishedArray = [];
-	var totalTasks = 0;
+$(document).on('ready', function () {
+    
+    $('#newTaskForm').hide();
 
-	var Task = function(task){
-		this.task = task;
-		this.id = totalTasks += 1;
-		this.symbol = '&#10148;';
-	};
+    var listo = [];
+    //var totalItems = listo.length;
 
+    var Task = function (task) {
+        this.task = task;
+        this.id = 'new';
+        this.symbol = '&#10148;';
+        //this.objId = (totalItems += 1) + task;
+    };
 
-	$('#newListItem').on('click',function() {
-		$('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
-	});
+    var addTask = function (task) {
+        listo.push(task);
+        localStorage["listo"] = JSON.stringify(listo);
+    };
 
-	$('#cancel').on('click', function(e) {
-		e.preventDefault();
-		$('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
-	});
-
-	var newObj = {};
-	$('#saveNewItem').on('click', function(e) {
-		e.preventDefault();
-		var newItem = $('#newItemInput').val();
-		newObj = new Task(newItem);
-			 if(newItem) {
-			newListArray.push(newObj);
-		 	console.log(newListArray);
-		 	$('#newList').append('<a href="#finish" class="" id="item"><li class="list-group-item">'+ newObj.task + '<span class="arrow pull-right">' +newObj.symbol+'</span></li></a>');
-		 	$('#newItemInput').val('');
-		 	$('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');	
-		}
-	});
+    var save = function() {
+        localStorage["listo"] = JSON.stringify(listo);
+    };
 
 
-$(document).on('click', '#item', function (e) {
-		e.preventDefault();
-		console.log(this);
-		this.id = "finishTask";
-		$('#currentList').append(this.outerHTML);
+    $('#newListItem').on('click', function () {
+        $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
+    });
 
-		this.remove();
-});
+    $('#cancel').on('click', function (e) {
+        e.preventDefault();
+        $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
+    });
 
-$(document).on('click', '#finishTask', function (e) {
-debugger;	
-		e.preventDefault();
-		console.log(this);
-		this.id = "archived";
-		this.className = "archived"
-		$('#archivedList').append(this.outerHTML);
-
-		this.remove();
-})
-
-$(document).on('click', '#archived', function (e) {
-		e.preventDefault();
-		this.remove();
-})
+    
+    $('#saveNewItem').on('click', function (e) {
+        e.preventDefault();
+        var newItem = $('#newItemInput').val();
+        var task = {};
+        if (newItem) {
+            task = new Task(newItem);
+            addTask(task);
+            $('#newItemInput').val('');
+            $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
+            $('#newList').append('<a href="#finish" class="" id="item"><li class="list-group-item">' + task.task + '<span class="arrow pull-right">' + task.symbol + '</span></li></a>');
+        }
+    });
 
 
+    var populateLists = function () {
+        var storedList = JSON.parse(localStorage.getItem("listo"));
+        for (var i = 0; i < storedList.length; i++) {
+            if (storedList[i].id === 'new') {
+                $('#newList').append('<a href="#finish" class="" id="item"><li class="list-group-item">' + storedList[i].task + '<span class="arrow pull-right">' + storedList[i].symbol + '</span></li></a>');
+            } else if (storedList[i].id === 'inProgress') {
+                $('#currentList').append('<a href="#finish" class="" id="inProgress"><li class="list-group-item">' + storedList[i].task + '<span class="arrow pull-right">' + storedList[i].symbol + '</span></li></a>');
+            } else {
+                $('#archivedList').append('<a href="#finish" class="" id="archived"><li class="list-group-item">' + storedList[i].task + '<span class="arrow pull-right">' + storedList[i].symbol + '</span></li></a>');
+            }
+        }
+    };
 
-
-
-$('.container').bind('DOMSubtreeModified', function (e) {
-    if (e.target.innerHTML.length > 0) {
-    	$(".list-group-item").mouseenter(function(){
-		$(this).find('.arrow').animate({ marginRight: '0px'}, 100)
-			}).mouseleave(function(){
-				$(this).find('.arrow').stop().css('marginRight', '100px')
-			}).click(function(){
-				$(this).find('.arrow').stop().css('marginRight', '100px');
-			});
+    if (localStorage.getItem("listo")) {
+        listo = JSON.parse(localStorage["listo"]);
+        populateLists();
     }
+
+    $(document).on('click', '#item', function (e) {
+        e.preventDefault();
+        debugger;
+        var modified = this.innerText.slice(0, this.innerText.length - 2);
+        
+        for (var i = 0; i < listo.length; i++) {
+            if (listo[i].task === modified) {
+                listo[i].id = 'inProgress';
+                save();
+                break;
+            }
+        }
+        this.id = "inProgress";
+        $('#currentList').append(this.outerHTML);
+        this.remove();
+    });
+
+    $(document).on('click', '#inProgress', function (e) {
+        e.preventDefault();
+        var modified = this.innerText.slice(0, this.innerText.length - 2);
+
+        for (var i = 0; i < listo.length; i++) {
+            if (listo[i].task === modified) {
+                listo[i].id = 'archived';
+                save();
+                break;
+            }
+        }
+        this.id = "archived";
+        $('#archivedList').append(this.outerHTML);
+        this.remove();
+    });
+
+    $(document).on('click', '#archived', function (e) {
+        e.preventDefault();
+        var modified = this.innerText.slice(0, this.innerText.length - 2);
+
+        for (var i = 0; i < listo.length; i++) {
+            if (listo[i].task === modified) {
+                listo.splice(i,1);
+                save();
+                break;
+            }
+        }
+        this.remove();
+    });
+
+    $('.container').bind('DOMSubtreeModified', function (e) {
+        if (e.target.innerHTML.length > 0) {
+            $(".list-group-item").mouseenter(function () {
+                $(this).find('.arrow').animate({ marginRight: '0px' }, 100)
+            }).mouseleave(function () {
+                $(this).find('.arrow').stop().css('marginRight', '100px')
+            }).click(function () {
+                $(this).find('.arrow').stop().css('marginRight', '100px');
+            });
+        }
+    });
+
 });
-
-
-
-});	
-
